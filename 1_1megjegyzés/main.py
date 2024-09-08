57a258f71498aeb0091174ba899c7f5ce1278bb7
Reni98 <93825160+Reni98@users.noreply.github.com>
2024-09-08 09:40:52 +0200

    Add files via upload

---
+++ 1_1megjegyzés/main.py
@@ -0,0 +1,52 @@
+# Egysoros megjegyzés
+# Egysoros megjegyzéseket a # karakterrel kezdünk. Ezek a karakterek utáni szövegrészletek teljes egészében a Python interpretáló által figyelmen kívül hagyatnak, így nem befolyásolják a program futását.
+
+print("Hello, World!")
+
+# Többsoros megjegyzés
+# Többsoros megjegyzéseket három idézőjellel (egy vagy dupla) kezdünk és zárunk. Ezek a megjegyzések több soron keresztül terjedhetnek, és gyakran használatosak hosszabb magyarázatok, dokumentációk, vagy ideiglenesen kikommentelt kódrészletek esetén.
+
+"""
+Ez egy többsoros megjegyzés.
+Itt több soron keresztül magyarázatokat vagy dokumentációt írhatunk.
+"""
+
+'''
+Ez is egy többsoros megjegyzés.
+Lehetőség van egyszeres idézőjelet is használni.
+'''
+
+# Példa többsoros megjegyzésre:
+"""
+Ez a kód egy egyszerű példa arra, hogyan lehet kiírni
+a szöveges üzenetet a képernyőre Pythonban.
+"""
+
+print("Hello, World!")
+
+
+# Megjegyzések célszerű használata
+# Magyarázatok és dokumentáció: Magyarázatokat és dokumentációt adhatunk a kódunkhoz, hogy más fejlesztők vagy mi magunk később könnyebben értsük meg, hogy mi történik a programban.
+
+# Debugging: Ideiglenes kikommentelt kódrészleteket használhatunk, hogy kikapcsoljuk bizonyos sorokat vagy blokkokat, amíg tesztelünk vagy hibakeresést végzünk.
+
+# Feladatok és teendők: Megjegyzéseket használhatunk arra is, hogy feljegyezzük a programon belüli feladatokat vagy további teendőket, amiket később el kell végezni.
+
+# Ez a program kiírja a "Hello, World!" üzenetet a képernyőre
+print("Hello, World!")
+
+"""
+Ez a kód egy példa arra, hogyan lehet számokat összeadni Pythonban.
+x = 5
+y = 10
+result = x + y
+print(result)
+"""
+
+# TODO: Implementáljuk a fájlból olvasást!
+
+# A kód alábbi része egy üdvözlő üzenetet ír ki a képernyőre
+name = "Alice"  # Az 'Alice' változóban tároljuk a névét
+print(f"Üdvözöllek, {name}!")
+
+# A megjegyzések segítenek abban, hogy a kódunk strukturáltabb legyen, könnyebben érthető, és könnyebb legyen rajta dolgozni másoknak vagy saját magunknak is később. Fontos, hogy a megjegyzések célszerű és konszisztens módon legyenek használva a kódolás során.
