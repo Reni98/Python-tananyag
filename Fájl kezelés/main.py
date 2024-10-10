43212df6e5a93fbf3fdb44e41d9fd7a7bfa2a0ba
Reni98 <93825160+Reni98@users.noreply.github.com>
2024-10-10 11:40:29 +0200

    Update main.py

--- Fájl kezelés/main.py
+++ Fájl kezelés/main.py
@@ -1,35 +1,78 @@
-#Fájlba írás
-# Fájl megnyitása írásra:
+import random
+import string
+#1.Feladat
+#Sorsolás: Készíts egy egyszerű sorsolást, ahol a felhasználók nevei közül választhatsz ki véletlenszerűen egyet.
+# nevek = ['Anna', 'Béla', 'Cecil', 'Dávid', 'Emma']
+# nev = random.choice(nevek)
+# print(f"A választott név: {nev}")
+# print(f"A választott nevek: {random.sample(nevek,2)}")
 
-# Egy fájl megnyitásához használjuk az open() függvényt.
-# Az első paraméter a fájl neve, a második az üzemmód (itt: 'w' - írás).
-# Ha a fájl nem létezik, a program létrehozza. Ha már létezik, felülírja a tartalmát.
+#2.Feladat
+#Véletlenszerű mondatok generálása: Készíts egy egyszerű mondatgenerátort, amely véletlenszerűen kiválaszt szavakat különböző listákból.
+# alany = ['A macska', 'A kutya', 'A kisfiú', 'A tanár']
+# ige = ['fut', 'ugrik', 'alszik', 'játszik']
+# targy = ['a labdával', 'a fűben', 'a házban', 'a barátokkal']
+# print(f"{random.choice(alany)} {random.choice(ige)} {random.choice(targy)}")
 
-fajl = open('szamok.txt', 'w')  # Fájl megnyitása írásra
-# Adatok írása a fájlba:
+# 3.Feladat
+# Jelszó generálása
+karakterek = string.ascii_letters + string.digits
+jelszo = "".join(random.choice(karakterek) for i in range(8))
+print(jelszo)
 
-# A következő példában a program 1-től 10-ig számokat ír a fájlba, 10 sorban, minden sorban 10 számot.
-# Az íráshoz a print() függvényt használjuk, ahol megadjuk a fájl nevét (file=fajl), ahová írni szeretnénk.
-for i in range(10):
-    sor = [str(i*10 + i1) for i1 in range(1, 11)]
-    print(' '.join(sor), file=fajl)  # A sorokat a fájlba írja
-# Fájl bezárása:
+with open("password.txt","w") as file:
+    file.write(f"{jelszo}; ")
 
-# A fájl írása után mindig zárjuk be a fájlt a close() függvénnyel, hogy biztosak legyünk abban, hogy az adatok valóban elmentődtek.
-fajl.close()  # Fájl lezárása
+# 4.Feladat
+# kő papír olló
+# lista=['kő','papír','olló']
+# gep = random.choice(lista)
+# user = input("Válassz egyet(kő papír olló):")
 
-# Fájl olvasása
-# Fájl megnyitása olvasásra:
+# if user == gep:
+#     print("Döntetetlen.")
+# elif user == "kő" and gep == "olló" or user == "olló" and gep=="papír" or user== "papír" and gep == "kő":
+#     print("Győztél!")
+# else:
+#     print("Vesztettél!")
 
-# Olvasáshoz használjuk az open() függvényt 'r' üzemmóddal (olvasás).
-fajl = open('szamok.txt', 'r')  # Fájl megnyitása olvasásra
-# Adatok olvasása a fájlból:
+# print(f"A gép választotta: {gep}, választásod: {user}")
 
-# A readlines() függvény minden sort beolvas, és egy listát ad vissza, ahol minden sor egy listaelem.
-sorok = fajl.readlines()  # Sorok beolvasása a fájlból
-for sor in sorok:
-    print(sor.strip())  # A sorokat kiírjuk a képernyőre, a felesleges szóközöket eltávolítva
-# Fájl lezárása:
+# 5.Feladat
+# Készíts egy programot ami bekér 1 és 200 között 15db számot és hozzá kell adni a számokat egy listához ha páros,
+# és addig kell a listához adni a számokat, amíg nincs meg az 15db.
+# Írasd ki a lista hosszát, hogy le tudjuk ellenőrizni, hogy meg van-e az 15 db.
+# Írd ki a számokat pontos vesszővel elválasztva.
 
-# Olvasás után is zárjuk be a fájlt.
-fajl.close()  # Fájl lezárása olvasás után
+# szamok = []
+# while len(szamok)<15:
+#     szam = int(input("Adj meg egy számot 1-200 között: "))
+#     if szam % 2 == 0:
+#         szamok.append(szam)
+
+# print(f"A lista hossza: {len(szamok)}.")
+# print('; '.join(map(str,szamok)))
+
+# szamok = []
+# while len(szamok)<15:
+#     szam = random.randint(1,200)
+#     if szam % 2 == 0:
+#         szamok.append(szam)
+
+# print(f"A lista hossza: {len(szamok)}.")
+# print('; '.join(map(str,szamok)))
+
+# with open("numbers.txt", "w") as file:
+#     file.write('; '.join(map(str,szamok)))
+
+# fajl = open('szamok.txt', 'w')  # Fájl megnyitása írásra
+    
+# fajl.close()  
+
+with open("movies.txt", "r") as file:
+    lines = file.readlines()
+
+    for i in lines:
+        i = i.strip()
+        szoveg = i.split(",")
+        print(",".join(szoveg))
