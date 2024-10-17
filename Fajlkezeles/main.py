c594f2215d51c6b0328cd1f049c882b4c250edff
Reni98 <93825160+Reni98@users.noreply.github.com>
2024-10-17 10:03:20 +0200

    Update main.py

--- Fajlkezeles/main.py
+++ Fajlkezeles/main.py
@@ -12,19 +12,68 @@ import random
 # A tipp F, a dobás eredménye I volt.
 # Ön nem találta el!
 
-dobas=["F","I"]
-dobasok=[]
-for i in range(5):
-    dob = random.choice(dobas)
-    dobasok.append(dob)
-print(" ".join(dobasok))
-feldobas= random.choice(dobasok)
-tipp = input("Kérek egy tippet (F/I):")
-with open("penzdobas.txt", "a") as file:
-    file.write(f"A tipp {tipp},a dobás eredménye {feldobas} volt.")
-    if tipp == feldobas:
-        file.write("Eltaláltad!")
-        print("Eltaláltad!")
-    else:
-        file.write("Nem találtad el!")
-        print("Nem találtad el!")
+# dobas=["F","I"]
+# dobasok=[]
+# for i in range(5):
+#     dob = random.choice(dobas)
+#     dobasok.append(dob)
+# print(" ".join(dobasok))
+# feldobas= random.choice(dobasok)
+# tipp = input("Kérek egy tippet (F/I):")
+# with open("penzdobas.txt", "a") as file:
+#     file.write(f"A tipp {tipp},a dobás eredménye {feldobas} volt.")
+#     if tipp == feldobas:
+#         file.write("Eltaláltad!")
+#         print("Eltaláltad!")
+#     else:
+#         file.write("Nem találtad el!")
+#         print("Nem találtad el!")
+# names=[]
+# with open("nevek.txt","r") as file:
+#     lines= file.readline().split(";")
+
+#     for line in lines:
+#         line = line.strip()       
+#         names.append(line)
+
+
+# names.sort()
+# print(" ".join(names))
+
+# with open("rendezett_nevek.txt","w") as fajl:
+#     fajl.write(" ".join(names))
+
+# Olvassuk be a szamok.txt fájlból az adatokat. Majd mentsük el a fájlba de már növekvő sorrendbe.
+# numbers = []
+# with open("random_szamok.txt", "r") as file:
+#     lines = file.readlines()
+#     for i in lines:
+#         i = i.strip()
+#         szam = i.split(",")
+#         numbers.extend(szam)
+
+#     numbers=[int(num) for num in numbers]
+
+#     numbers.sort()
+#     with open("rendezett_random_szamok.txt", "w") as fajl:
+#         fajl.write(",".join(map(str, numbers)))
+
+names = []
+with open("osztalyatlag.txt", "r") as file:
+    lines = file.readlines()
+    for i in lines:
+        i = i.strip()
+        nev,atl = i.split(";")
+        names.append((nev,float(atl)))
+
+
+negyesfelettiek=[]
+eredmenyek = 0
+for nev, atl in names:
+    eredmenyek += atl
+    if atl > 4:
+        negyesfelettiek.append(nev)
+print(negyesfelettiek)
+with open("atlag.txt", "w") as fajl:
+    fajl.write(f"Az átlag: {eredmenyek/len(names)}")
+    fajl.write(";".join(map(str,negyesfelettiek)))
